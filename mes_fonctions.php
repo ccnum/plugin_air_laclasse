62bcca2f2d83b7790d3d8a94a2be18372696fbb9
paracine <pierrealexandreracine@gmail.com>
2022-11-15 15:09:15 +0100

    test entête dynamique

--- mes_fonctions.php
+++ mes_fonctions.php
@@ -6,7 +6,9 @@ function enleverParenthesesTexte($texte=''): string
         if ($texte[0] === '(') {
             $texte = substr($texte, 1, strlen($texte) - 1);
         }
+        if ($texte[-1] === ')') {
+            $texte = substr($texte, 0, -1);
+        }
     }
-
     return $texte;
 }

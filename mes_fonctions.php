79f9a1672772e8f353ac49e91f00ad263384df88
paracine <pierrealexandreracine@gmail.com>
2022-11-15 15:12:18 +0100

    doc fonction parenthèse

--- mes_fonctions.php
+++ mes_fonctions.php
@@ -16,7 +16,7 @@ function enleverParenthesesTexte(string $texte=''): string
         if ($texte[0] === '(') {
             $texte = substr($texte, 1, strlen($texte) - 1);
         }
-        if ($texte[-1] === ')') {
+        if (strlen($texte) > 1 && $texte[-1] === ')') {
             $texte = substr($texte, 0, -1);
         }
     }
